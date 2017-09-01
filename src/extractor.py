c14cf650a7ff8dc4846903473827d7fc1bbf1964
trungkak <trungkak@gmail.com>
2017-09-01 17:48:00 +0700

    moving on extracting product page

--- src/extractor.py
+++ src/extractor.py
@@ -1,10 +1,39 @@
 from bs4 import BeautifulSoup
+from lxml.etree import tostring
+import lxml.html
 
 class Extractor(object):
     """
     Extract data from HtmlElement
+    Data includes:
+        - Link
+        - Author
+        - Title
+        - Text/Description
+        - Price
+        - Image
+        - Date time
+        - Rating
+        - Comments
     """
-    def __init__(self, node):
+    def __init__(self, doc, type='a'):
         """
-        :param node: lxml HtmlElement object
+        doc: html source
+        type: 'a' => article; 'c' => comment
         """
+        self.type = type
+        self.doc = doc
+        self.parser = lxml.html.fromstring(self.doc)
+
+    def parse(self):
+        soup = BeautifulSoup(tostring(self.doc))
+
+    def get_title_for_article(self):
+        """
+        Step1: find title element, if title is found: return title
+        Step2: find all h1 tag
+        """
+        title = ''
+        title_elem = self.parser.find('.//title').text
+        if title_elem is None or title_elem == '':
+            return title
